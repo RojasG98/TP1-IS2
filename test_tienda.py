683d092fcb3ec06b5f46752f1bf90ad7d0232670
GabielR98 <rojista.098g@gmail.com>
2025-10-11 16:56:18 -0300

    Realizacion ejercicio 2

--- test_tienda.py
+++ test_tienda.py
@@ -1,3 +1,4 @@
+import pytest
 from tienda import Tienda
 from producto import Producto
 
@@ -56,3 +57,41 @@ def test_eliminar_producto_no_existente():
 
     assert resultado_eliminacion is False
     assert len(tienda.inventario) == 1
+
+def test_actualizar_producto_existente():
+    tienda = Tienda()
+    producto_nuevo = Producto("Arroz", 1.00, "Granos")
+    tienda.agregar_producto(producto_nuevo)
+
+    resultado_actualizacion = tienda.actualizar_producto("Arroz", 1.20)
+
+    assert resultado_actualizacion is True
+    producto_actualizado = tienda.buscar_producto("Arroz")
+    assert producto_actualizado.precio == 1.20
+
+def test_actualizar_producto_no_existente():
+    tienda = Tienda()
+    producto_nuevo = Producto("Frijoles", 1.50, "Granos")
+    tienda.agregar_producto(producto_nuevo)
+
+    resultado_actualizacion = tienda.actualizar_producto("Lentejas", 1.80)
+
+    assert resultado_actualizacion is False
+    producto_no_actualizado = tienda.buscar_producto("Frijoles")
+    assert producto_no_actualizado.precio == 1.50
+
+def test_excepcion_en_buscar_producto():
+    tienda = Tienda()
+    with pytest.raises(ValueError):
+        tienda.buscar_producto("NoExiste") 
+
+def test_excepcion_en_eliminar_producto():
+    tienda = Tienda()
+    with pytest.raises(ValueError):
+        tienda.eliminar_producto("NoExiste")
+
+def test_excepcion_en_actualizar_producto():
+    tienda = Tienda()
+    with pytest.raises(ValueError):
+        tienda.actualizar_producto("NoExiste", 10.0)
+
--- tienda.py
+++ tienda.py
@@ -9,11 +9,27 @@ class Tienda:
         for producto in self.inventario:
             if producto.nombre == nombre:
                 return producto
-        return None
+        raise ValueError("Producto no encontrado")
+        
     def eliminar_producto(self,nombre):
         for producto in self.inventario:
             if producto.nombre == nombre:
                 self.inventario.remove(producto)
                 return True
-        return False
+        raise ValueError("Producto no eliminado")
     
+    def actualizar_producto(self,nombre, nuevo_precio):
+        if nuevo_precio>=0:
+            for producto in self.inventario:
+                if producto.nombre == nombre:
+                    producto.precio = nuevo_precio
+                    return True
+        raise ValueError("Producto no actualizado")
+    def aplicar_descuento(self,nombre, porcentaje):
+        if 0 <= porcentaje <= 100:
+            producto = self.buscar_producto(nombre)
+            descuento = producto.precio * (porcentaje / 100)
+            producto.precio -= descuento    
+            return True
+        else:
+            raise ValueError("Porcentaje de descuento inválido")
